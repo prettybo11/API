20e1266ed0e932fa718e8efba28b6d6256803d43
79537 <79537@LAPTOP-RH79JEQ2>
2026-01-15 17:05:31 +0700

    Добавление API-контроллера

---
+++ CollegeSchedule/Controllers/ScheduleController.cs
@@ -0,0 +1,24 @@
+﻿using CollegeSchedule.Data;
+using CollegeSchedule.Models;
+using CollegeSchedule.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+namespace CollegeSchedule.Controllers
+{
+    [ApiController]
+    [Route("api/schedule")]
+    public class ScheduleController : ControllerBase
+    {
+        private readonly IScheduleService _service;
+        public ScheduleController(IScheduleService service, AppDbContext db)
+        {
+            _service = service;
+        }
+        [HttpGet("group/{groupName}")]
+        public async Task<IActionResult> GetSchedule(string groupName, DateTime start, DateTime end)
+        {
+            var result = await _service.GetScheduleForGroup(groupName, start.Date, end.Date);
+            return Ok(result);
+        }
+    }
+}
